6ef1109abaab803d5c4a98ba051370710c6bd12a
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-09-26 16:14:16 +0200

    The callback function

--- ES6-ES2015/Promises.js
+++ ES6-ES2015/Promises.js
@@ -59,24 +59,41 @@
 
 //  // video2: https://www.youtube.com/watch?v=rdVG85LGsVE&list=PLIJrr73KDmRyCanrlIS8PEOF0kPKgI8jN
 
-//Example5: Promise and handler methods
-const promise = new Promise((resolve, reject) => {
-  //   //Resolve
-  //   const value = "water";
-  //   resolve(value);
-  //Reject
-  reject(new Error("The Jack and Jill has fallen down on mountain"));
-});
+// //Example5: Promise and handler methods
+// const promise = new Promise((resolve, reject) => {
+//   //   //Resolve
+//   //   const value = "water";
+//   //   resolve(value);
+//   //Reject
+//   reject(new Error("The Jack and Jill has fallen down on mountain"));
+// });
 
-function grandParentCooking() {
-  promise
-    .then(function (result) {
-      console.log(`The ${result} is available you're able to cook!!!`);
-    })
-    .catch(function (error) {
-      console.log(`Opps: ${error.message}`);
-    })
-    .finally(() => console.log("Developed by : Alicade"));
+// function grandParentCooking() {
+//   promise
+//     .then(function (result) {
+//       console.log(`The ${result} is available you're able to cook!!!`);
+//     })
+//     .catch(function (error) {
+//       console.log(`Opps: ${error.message}`);
+//     })
+//     .finally(() => console.log("Developed by : Alicade"));
+// }
+
+// grandParentCooking();
+
+//Video 3: https://www.youtube.com/watch?v=AUCavCH7FTw&list=PLIJrr73KDmRyCanrlIS8PEOF0kPKgI8jN&t=800s
+// Example 6: useCallback
+
+function orderPizza(flavor, type, callback) {
+  console.log(`The ${flavor} ${type} is ordered`);
+
+  setTimeout(function () {
+    const msg = `The ${flavor} ${type} is ready`;
+    // console.log(msg);
+    callback(msg);
+  }, 3000);
 }
 
-grandParentCooking();
+orderPizza("Margherita", "Greekpizza", function (msg) {
+  console.log(msg);
+});

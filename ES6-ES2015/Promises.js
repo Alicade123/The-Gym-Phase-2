58ed0b111c7b6e6137aa6a3d8cfee64c30d0778e
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-09-26 12:18:01 +0200

    Async learning - ES6

--- ES6-ES2015/Promises.js
+++ ES6-ES2015/Promises.js
@@ -6,16 +6,33 @@
 // f1();
 // f2();
 // f3();
-// Example 2: synchronous
+// // Example 2: synchronous
+// function f1() {
+//   //Function definition
+// }
+// function f2() {
+//   //Function definition
+//   f1();
+// }
+// function f3() {
+//   //Function definition
+//   f2();
+// }
+// f3();
+
+// Example 3: Async
 function f1() {
-  //Function definition
+  console.log("f1");
 }
 function f2() {
-  //Function definition
-  f1();
+  console.log("f2");
 }
-function f3() {
-  //Function definition
+function main() {
+  console.log("main");
+
+  setTimeout(f1, 0);
+
   f2();
 }
-f3();
+
+main();

1b57f308074de5c306a46b3394c8fcd6d9b4eda8
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-09-26 12:16:49 +0200

    Asynchronous learning part2 - ES6

--- ES6-ES2015/Promises.js
+++ ES6-ES2015/Promises.js
@@ -1,8 +1,21 @@
-// Example 1: synchronous
-function f1() {}
-function f2() {}
-function f3() {}
+// // Example 1: synchronous
+// function f1() {}
+// function f2() {}
+// function f3() {}
 
-f1();
-f2();
+// f1();
+// f2();
+// f3();
+// Example 2: synchronous
+function f1() {
+  //Function definition
+}
+function f2() {
+  //Function definition
+  f1();
+}
+function f3() {
+  //Function definition
+  f2();
+}
 f3();
